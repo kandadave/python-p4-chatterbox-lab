c826cf1e4c887c6ff7d2cae7c4467987c601ea8f
kandadave <kandachumo@gmail.com>
2025-10-02 17:48:52 +0300

    chatterbox backend

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 
 from models import db, Message
+from sqlalchemy import func  # For any datetime funcs if needed in routes (model handles most)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -16,11 +17,41 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    all_messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([msg.to_dict() for msg in all_messages])
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    if 'body' in data:
+        message.body = data['body']
+   
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
